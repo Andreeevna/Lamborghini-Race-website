08ca98b2997ebc2fe6c2716ffc0a93665f8f3a72
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-08-31 12:33:59 +0300

    feat: implemented a slider in the blog block

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	loop: true,
+	slidesPerView: 2,
+	spaceBetween: 20,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-left',
+		prevEl: '.swiper-button-right',
+	},
+})
